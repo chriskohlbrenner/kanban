cd5cdbb3e2066ef6fbe6910cf7a5da1282669b18
chriskohlbrenner <c.kohlbrenner@gmail.com>
2015-12-09 15:17:38 -0500

    through chapter 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,46 @@
 var path = require('path');
-var htmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+var common = {
   entry: PATHS.app,
-  output: {
-    path: PATHS.build,
-    filename: 'bundle.js'
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+        include: PATHS.app
+      }
+    ]
   },
   plugins: [
-    new htmlWebpackPlugin({
+    new HtmlwebpackPlugin({
       title: 'Kanban app'
     })
   ]
 };
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
