a450bedffdb9430cb9d295269daf25b8804463e8
chriskohlbrenner <c.kohlbrenner@gmail.com>
2016-01-11 08:25:08 -0500

    5.4 use store and actions instead of component state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,16 +1,28 @@
-import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
-import AppState from '../State.js'
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = AppState
+    this.state = NoteStore.getState()
     this.addNote = this.addNote.bind(this);
     this.editNote = this.editNote.bind(this);
     this.deleteNote = this.deleteNote.bind(this);
-    this.findNote = this.findNote.bind(this);
+    this.storeChanged = this.storeChanged.bind(this);
+  }
+
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+
+  storeChanged(state) {
+    this.setState(state);
   }
 
   render() {
@@ -24,43 +36,15 @@ export default class App extends React.Component {
   }
 
   addNote() {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New'
-      }])
-    })
+    NoteActions.create({task: 'New Task'});
   }
 
   editNote(id, task) {
-    const notes = this.state.notes;
-    const noteIndex = this.findNote(id);
-    if(noteIndex < 0) {
-      return;
-    }
-    notes[noteIndex].task = task;
-    this.setState({notes: notes});
+    NoteActions.update({id, task});
   }
 
   deleteNote(id) {
-    const notes = this.state.notes;
-    const noteIndex = this.findNote(id);
-
-    if (noteIndex < 0) {
-      return;
-    }
-    this.setState({
-      notes: notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1))
-    });
-  }
-
-  findNote(id) {
-    const notes = this.state.notes;
-    const noteIndex = notes.findIndex((note) => note.id === id);
-    if(noteIndex < 0) {
-      console.warn('Failed to find note', notes, id);
-    }
-    return noteIndex;
+    NoteActions.delete(id);
   }
 
 }
